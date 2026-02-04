6a452d8e111ccf835b1216a35f3a0549a3d9502d
westzero0 <89689577+westzero0@users.noreply.github.com>
2026-02-04 12:13:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycby6jI-g4YUueGPd_WcrN0OUf0uE5nj4AeM0KhdACfIoXPXBJ54vAzYk7sZLnLoeMivo/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbw-piW9Gkcw6lTBis7o7bFTIWJDPuBXtWOzNsrxdpA9oyBoODpW6M3Z2WjMrRad48sI/exec";
 
 
 // 💡 1. 통합 초기 로드 로직
