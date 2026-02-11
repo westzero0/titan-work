9f6cdb5f02aaa2108516796232d9dcc1bda55890
westzero0 <89689577+westzero0@users.noreply.github.com>
2026-02-11 23:02:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxtQQMC9fe7EEicnjToJukaicFnevLasg0nRNPspKTrmEz4e5xLRk0nhN7TrdE2m1uG/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbw_zKyCAuCOpdr2oHD0wxL9iAJ_d5ALkP4zA6G7B0rnaAXAF4wE4_mSZ_4bY7j0PUMp/exec";
 
 
 // 💡 1. 통합 초기 로드 로직
