ea12fe6f226cc010699e33a33a6a24b210288c8e
westzero0 <89689577+westzero0@users.noreply.github.com>
2026-03-10 23:31:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbw1uZmBvRnbuR9Dgv2a0qu9Ko2kzYi3QWoaK4E-qiNrwPsy3JbP8gf90zYBAzX4J628/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz4Pl186OaqgjuOnCyGzROBIBGEpAH2PKU0sMhhWrBhPmQQ56ylj8-XOdZboguSms45/exec";
 
 var globalTitanData = globalTitanData || {}; // 👈 변수가 없으면 빈 박스라도 만들어라!
 
