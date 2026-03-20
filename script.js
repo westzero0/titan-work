abf656f3dfdd75fe64085ed982a1c21f8998c758
westzero0 <89689577+westzero0@users.noreply.github.com>
2026-03-20 12:53:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js');
+}
+
 const APP_VERSION = "2.1"; // 👈 기능 수정할 때마다 이 숫자를 1.6, 1.7로 올리세요!
 
 document.addEventListener('DOMContentLoaded', () => {
