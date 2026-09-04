9b15be7383ae9c10b72d1493d197f3b9694e2999
westzero0 <89689577+westzero0@users.noreply.github.com>
2026-09-04 20:03:08 +0900

    Fix syntax errors in service worker file

--- sw.js
+++ sw.js
@@ -1,15 +1,20 @@
-// sw.js (파일 이름은 반드시 sw.js여야 함)
+// sw.js - filename must be sw.js
 self.addEventListener('install', (e) => {
  e.waitUntil(
-   caches.open('titan-v1').then((cache) => cache.addAll([
-     './index.html',
-     './192.png'
+  caches.open('titan-v1').then((cache) => cache.addAll([
+   './index.html',
+   './192.png'
    ]))
- );
+  );
 });
 
 self.addEventListener('fetch', (e) => {
+ const url = new URL(e.request.url);
+ const isSameOrigin = url.origin === self.location.origin;
+ if (e.request.method !== 'GET' || !isSameOrigin) {
+  return;
+ }
  e.respondWith(
-   caches.match(e.request).then((response) => response || fetch(e.request))
- );
+  caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
